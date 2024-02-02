aef714b4d6630a465e1eda0e54ebde84c9485962
David de Souza Alves <david.alves@consorciocq.com.br>
2024-02-02 17:39:15 -0300

    Filtro de livros disponiveis

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,8 +1,10 @@
 const botoes = document.querySelectorAll('.btn')
 
-botoes.forEach( botao => botao.addEventListener('click', () => filtraLivros(botao.dataset.categoria)));
-
-function filtraLivros(categoria){
-    let livrosFiltrados = livros.filter( livro => livro.categoria == categoria)
+botoes.forEach( botao => botao.addEventListener('click', () => filtraLivros(botao.value)));
+ 
+function filtraLivros(valorBotao){
+    // const elementoBtn = document.getElementById(this.id)
+    // const categoria = elementoBtn.val
+    let livrosFiltrados = valorBotao == 'disponivel' ? livros.filter(livro => livro.quantidade > 0 ) : livros.filter( livro => livro.categoria == valorBotao)
     exibirLivrosNaTela(livrosFiltrados)
 }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,21 +1,25 @@
 const elementoParaInserir = document.getElementById('livros')
 
-function exibirLivrosNaTela(listaDeLivros){
+function exibirLivrosNaTela(listaDeLivros) {
     elementoParaInserir.innerHTML = '';
     listaDeLivros.forEach(livro => {
+
+        let disponibilidade = livro.quantidade > 0 ? '' : 'indisponivel'
+
         elementoParaInserir.innerHTML += `  
         <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}"
-        alt="${livro.alt}">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>
+            <img class="livro__imagens ${disponibilidade}" src="${livro.imagem}"
+            alt="${livro.alt}">
+                ${livro.titulo}
+                </h2>
+                <p class="livro__descricao">${livro.autor}</p>
+                <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+            <div class="tags">
+                <span class="tag">${livro.categoria}</span>
+            </div>
+        </div>
         `
+
     })
 
 }
